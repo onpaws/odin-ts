d096fc727b803e4bb1cddf71fcef9489cc215ddb
Pat <me@onpaws.com>
2018-10-23 18:50:07 -0300

    hello world in TS

--- src/Goodbye.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-
-interface GoodbyeProps { compiler: string; framework: string; }
-
-export const Goodbye = (props: GoodbyeProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
+import styles from './Hello.module.css';
 
 export interface HelloProps { compiler: string; framework: string; }
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+export const Hello = (props: HelloProps) =>
+  <div className={styles.hello}>
+    <h1>Hello from {props.compiler} and {props.framework}!</h1>
+  </div>
