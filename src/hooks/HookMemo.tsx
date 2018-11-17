afd6145ee3e99016346b9e7a076bf862ec9a6f53
Pat <me@onpaws.com>
2018-11-17 04:48:48 +0100

    hook memo demo

--- src/hooks/HookMemo.tsx
+++ src/hooks/HookMemo.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
-import {useMemo} from 'react';
+import {useState, useMemo} from 'react';
+
+const users = [
+  'bob',
+  'charles',
+  'ragnhild',
+  'patrick'
+];
 
 export const HookMemo = () => {
+  const [searchText, setSearchText] = useState('');
+  
+  const handleChange = (event: any) => {
+    setSearchText(event.target.value);
+  }
+  
   // 1st param is a callback function, it returns a value
   // useMemo accepts array for 2nd param. Think of it as the 'dependencies' of the hook
-  // const filteredUsers = useMemo(
-  //   () => filterUsers(users, searchText),
-  //   [users, searchText]
-  // )
+  const filterUsers = (users: Array<String>, searchText: string) => {
+    console.log('filterUsers() called')
+    return users.filter(user => user.match(searchText));
+  }
+  
+  const filteredUsers = useMemo(
+    () => filterUsers(users, searchText),
+    [users, searchText]
+  )
 
   return (
     <div>
-
+      <input value={searchText} onChange={handleChange} />
+      {filteredUsers && filteredUsers.map((user, index) => 
+        <p key={index}>{user}</p>
+      )}
     </div>
   )
 }
