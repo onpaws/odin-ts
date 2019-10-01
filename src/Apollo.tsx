9292fb3aa69a71da1c4ac3639404ef950dace6e5
Pat <me@onpaws.com>
2019-10-01 12:42:54 +0200

    access & refresh token support

--- src/Apollo.tsx
+++ src/Apollo.tsx
@@ -1,10 +1,42 @@
 import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloLink, split } from 'apollo-link';
+import { ApolloLink, split, Observable } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { WebSocketLink } from 'apollo-link-ws';
 import { onError } from 'apollo-link-error';
 import { getMainDefinition } from 'apollo-utilities';
+import { TokenRefreshLink } from 'apollo-link-token-refresh';
+import jwtDecode from 'jwt-decode';
+import { getAccessToken, setAccessToken } from './accessToken';
+
+const requestLink = new ApolloLink(
+  (operation, forward) =>
+    new Observable(observer => {
+      let handle: any;
+      Promise.resolve(operation).then(operation => {
+        const accessToken = getAccessToken();
+        if (accessToken) {
+          operation.setContext({
+            headers: {
+              Authorization: `Bearer ${accessToken}`
+            }
+          });
+        }
+      })
+      .then(() => {
+        handle = forward && forward(operation).subscribe({
+          next: observer.next.bind(observer),
+          error: observer.error.bind(observer),
+          complete: observer.complete.bind(observer)
+        });
+      })
+      .catch(observer.error.bind(observer));
+
+      return () => {
+        if (handle) handle.unsubscribe();
+      };
+    })
+);
 
 const httpLink = new HttpLink({
   uri: 'http://localhost:4000',
@@ -14,18 +46,22 @@ const httpLink = new HttpLink({
 const wsLink = new WebSocketLink({
   uri: 'ws://localhost:4000',
   options: {
-    reconnect: true,
-  },
+    reconnect: true
+  }
 });
 
-const networkLink = split(
-  // split based on operation type
+interface Definition {
+  kind: string;
+  operation?: string;
+};
+
+const networkLink = split(  // split based on operation type
   ({ query }) => {
-    const { kind, operation } = getMainDefinition(query);
-    return kind === 'OperationDefinition' && operation === 'subscription';
+    const { kind, operation }: Definition = getMainDefinition(query);
+      return kind === 'OperationDefinition' && operation === 'subscription';
   },
   wsLink,
-  httpLink,
+  httpLink
 );
 
 const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
@@ -46,7 +82,42 @@ const cache = new InMemoryCache();
 // Initialize the Apollo Client
 const client = new ApolloClient({
   link: ApolloLink.from([
+    new TokenRefreshLink({
+      accessTokenField: "accessToken",
+      isTokenValidOrUndefined: () => {
+        const token = getAccessToken();
+
+        if (!token) {
+          return true;
+        }
+
+        try {
+          const { exp } = jwtDecode(token);
+          if (Date.now() >= exp * 1000) {
+            return false;
+          } else {
+            return true;
+          }
+        } catch (err) {
+          return false;
+        }
+      },
+      fetchAccessToken: () => {
+        return fetch("http://localhost:4000/refresh_token", {
+          method: "POST",
+          credentials: "include"
+        })
+      },
+      handleFetch: accessToken => {
+        setAccessToken(accessToken);
+      },
+      handleError: err => {
+        console.warn("Your refresh token is invalid. Please try re-logging in.");
+        console.error(err);
+      }
+    }),
     errorLink,
+    requestLink,
     networkLink,
   ]),
   cache,
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
+import { setAccessToken } from './accessToken';
+import Me from './Me';
 
-const App = () =>
-  <div>
-    <Layout />
-  </div>
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("http://localhost:4000/refresh_token", {
+      method: "POST",
+      credentials: "include"
+    }).then(async response => {
+      const { accessToken } = await response.json();
+      setAccessToken(accessToken);
+      setLoading(false);
+    }).catch(err => {
+      console.error(err)
+    });
+  }, []);
+  
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div>
+      <Layout />
+      <Me />
+    </div>
+  )
+}
+
 
 export default App
