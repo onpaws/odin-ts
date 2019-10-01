3c54f84e187a8dbaced20a3fe117b2f6cb4d358a
Pat <me@onpaws.com>
2019-10-02 01:04:58 +0200

    use ApolloProvider from Apollo hooks module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider } from '@apollo/react-hooks';
 import './index.css';
 import App from './App';
 import Spinner from './spinner';
